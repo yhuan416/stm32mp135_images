926fabd24fffab281e6d5eeccaa9e0b0003454e2
yangzelin <yhuan416@foxmail.com>
2024-06-27 20:29:28 +0800

    feat: update

--- build_bootfs.sh
+++ build_bootfs.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# sudo apt-get install android-tools-fsutils
+
 mkdir -p bootfs
 cd bootfs
 
 rm stm32mp135d-atk* uImage 5.15.24/ -rf
 cp ../../linux/tmp/stm32mp135d-atk* ../../linux/tmp/uImage ./
 tar -axvf ../../linux/tmp/modules.tar.bz2
+
+# depend android-tools-fsutils
+cd ..
+make_ext4fs -l 64M bootfs.ext4 bootfs
